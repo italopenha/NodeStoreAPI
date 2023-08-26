4255e7407be92d9ac5805d555d055e412875794b
Ítalo Penha <italopenha77@outlook.com>
2023-08-25 23:07:59 -0300

    Refatorando função de atualizar produto

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -68,11 +68,24 @@ exports.getByTag = (req, res, next) => {
 }
 
 exports.put = (req, res, next) => {
-    const id = req.params.id;
-    res.status(200).send({
-        id: id,
-        item: req.body
-    });
+    Product
+        .findByIdAndUpdate(req.params.id, {
+            $set: {
+                title: req.body.title,
+                description: req.body.description,
+                price: req.body.price,
+                slug: req.body.slug
+            }
+        }).then(x => {
+            res.status(201).send({
+                message: 'Produto atualizado com sucesso!'
+            });
+        }).catch(e => {
+            res.status(400).send({
+                message: 'Falha ao atualizar o produto!',
+                data: e
+            });
+        });
 };
 
 exports.delete = (req, res, next) => {
