2d08d650350149598597d7505965a30610d24748
Ítalo Penha <italopenha77@outlook.com>
2023-08-25 23:53:33 -0300

    Criando o modelo de Cliente

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 // Carrega os models
 const Product = require('./models/product');
+const Customer = require('./models/customer');
 
 // Carrega as rotas
 const indexRoute = require('./routes/index-route');
