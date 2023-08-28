4059955d9dfe6dd358fb22de598b354a0c7f719a
Ítalo Penha <italopenha77@outlook.com>
2023-08-28 17:48:56 -0300

    Adicionando função de upload de imagem do produto

--- src/config.js
+++ src/config.js
@@ -5,5 +5,5 @@ global.SALT_KEY = process.env.SALT_KEY;
 
 module.exports = {
     connectionString: process.env.CONNECTION_STRING,
-    containerConnectionString: ''
+    containerConnectionString: process.env.CONTAINER_CONNECTION_STRING
 }
--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -2,6 +2,9 @@
 
 const ValidationContract = require('../validators/fluent-validator');
 const repository = require('../repositories/product-repository');
+const azurestorage = require('azure-storage');
+const config = require('../config');
+const guid = require('guid');
 
 exports.post = async (req, res, next) => {
     let contract = new ValidationContract();
@@ -15,11 +18,38 @@ exports.post = async (req, res, next) => {
     }
 
     try {
-        await repository.create(req.body);
+        // Cria o Blob Service
+        const blobSvc = azurestorage.createBlobService(config.containerConnectionString);
+
+        let filename = guid.raw().toString() + '.jpg';
+        let rawdata = req.body.image;
+        let matches = rawdata.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+        let type = matches[1];
+        let buffer = new Buffer(matches[2], 'base64');
+
+        // Salva a imagem
+        await blobSvc.createBlockBlobFromText('product-images', filename, buffer, {
+            contentType: type
+        }, function (error, result, response) {
+            if (error) {
+                filename = 'default-product.png'
+            }
+        }); 
+
+        await repository.create({
+            title: req.body.title,
+            slug: req.body.slug,
+            description: req.body.description,
+            price: req.body.price,
+            active: true,
+            tags: req.body.tags,
+            image: 'https://nodestore2.blob.core.windows.net/product-images/' + filename
+        });
         res.status(201).send({
             message: 'Produto cadastrado com sucesso!'
         });
     } catch (error) {
+        console.log(error);
         res.status(500).send({
             message: 'Falha ao processar requisição'
         });
